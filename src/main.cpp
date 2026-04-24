84ce7853b307d5093d535aa565e13a3552b5d1e4
toycodemaker <primer_plus202010@163.com>
2026-04-25 02:09:11 +0800

    Change source file format And make struct emu8051 a class

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@
  *
  * (i.e. the MIT License)
  *
- * main.cpp
+ * main.cpp(original emu.c)
  * Curses-based emulator front-end
  * ---------------------------------------------------------------------------
  * Significant refactoring and modifications by Liu Qitao (2026).
@@ -48,12 +48,10 @@
 #else
 #include "curses.h"
 #endif
-extern "C" {
+
 #include "emu8051.h"
-}
 
 //unsigned char history[HISTORY_LINES * (128 + 64 + sizeof(int))];
-
 
 // current line in the history cyclic buffers
 int historyline = 0;
@@ -71,73 +69,64 @@ unsigned int icount = 0;
 unsigned int clocks = 0;
 
 // old port out values
-int pout[4] = { 0 };
+int pout[4] = {0};
 
 int breakpoint = -1;
 
 // returns time in 1ms units
-int getTick()
-{
+int getTick() {
 #ifdef _MSC_VER
-    return GetTickCount();
+  return GetTickCount();
 #else
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    return now.tv_sec * 1000 + now.tv_usec / 1000;
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  return now.tv_sec * 1000 + now.tv_usec / 1000;
 #endif
 }
 
-void emu_sleep(int value)
-{
+void emu_sleep(int value) {
 #ifdef _MSC_VER
-    Sleep(value);
+  Sleep(value);
 #else
-    usleep(value * 1000);
+  usleep(value * 1000);
 #endif
 }
 
-
-
-void emu_sfrwrite_SBUF(struct em8051 *aCPU, uint8_t aRegister)
-{
-    aCPU->serial_out_remaining_bits = 8;
+void emu_sfrwrite_SBUF(struct em8051* aCPU, uint8_t aRegister) {
+  aCPU->serial_out_remaining_bits = 8;
 }
 
-uint8_t emu_sfrread(struct em8051 *aCPU, uint8_t aRegister)
-{
-  std::print("Unsupported Function: {}", __FUNCTION__ );
+uint8_t emu_sfrread(struct em8051* aCPU, uint8_t aRegister) {
+  std::print("Unsupported Function: {}", __FUNCTION__);
   return 0;
 }
 
-
-void hello(struct em8051 *aCPU, int changeto)
-{
+void hello(struct em8051* aCPU, int changeto) {
 }
 
-
-void show_emu(const em8051* aCPU)
-{
+void show_emu(const em8051* aCPU) {
   char ASMcommand[256]{};
   decode((em8051*)aCPU, aCPU->mPC, ASMcommand);
   //if (aCPU->mPC < 200)
-  std::println("PC: {:4x}, ASM Command:{}", 
-      aCPU->mPC, 
-      ASMcommand
-  );
+  std::println("PC: {:4x}, ASM Command:{}",
+               aCPU->mPC,
+               ASMcommand);
+  std::println("The Command delay Tick is {}", aCPU->mTickDelay);
+  if (aCPU->mTickDelay != 1)
+    std::cin.get();
 }
 
-int main(int parc, char ** pars)
-{
+int main(int parc, char** pars) {
   struct em8051 emu;
   int ticked = 1;
 
   memset(&emu, 0, sizeof(emu));
-  emu.mCodeMemMaxIdx = 65536-1;
-  emu.mCodeMem     = (unsigned char*)calloc(emu.mCodeMemMaxIdx+1, sizeof(unsigned char));
-  emu.mExtDataMaxIdx = 65536-1;
-  emu.mExtData     = (unsigned char*)calloc(emu.mExtDataMaxIdx+1, sizeof(unsigned char));
-  emu.mUpperData   = (unsigned char*)calloc(128, sizeof(unsigned char));
-  emu.except       = &hello;
+  emu.mCodeMemMaxIdx = 65536 - 1;
+  emu.mCodeMem = (unsigned char*)calloc(emu.mCodeMemMaxIdx + 1, sizeof(unsigned char));
+  emu.mExtDataMaxIdx = 65536 - 1;
+  emu.mExtData = (unsigned char*)calloc(emu.mExtDataMaxIdx + 1, sizeof(unsigned char));
+  emu.mUpperData = (unsigned char*)calloc(128, sizeof(unsigned char));
+  emu.except = &hello;
   emu.xread = NULL;
   emu.xwrite = NULL;
 
@@ -159,8 +148,7 @@ int main(int parc, char ** pars)
     targetclocks = 0;
     targettime = getTick();
 
-    do
-    {
+    do {
       /*
       if (opt_step_instruction)
       {
@@ -185,11 +173,9 @@ int main(int parc, char ** pars)
         ticked = tick(&emu);
       }
 
-    }
-    while (targettime > getTick() && targetclocks > 0);
+    } while (targettime > getTick() && targetclocks > 0);
 
-    while (targettime > getTick())
-    {
+    while (targettime > getTick()) {
       emu_sleep(1);
     }
 
@@ -200,57 +186,54 @@ int main(int parc, char ** pars)
   return EXIT_SUCCESS;
 }
 
-int readbyte(FILE * f)
-{
-    char data[3];
-    data[0] = fgetc(f);
-    data[1] = fgetc(f);
-    data[2] = 0;
-    return strtol(data, NULL, 16);
+int readbyte(FILE* f) {
+  char data[3];
+  data[0] = fgetc(f);
+  data[1] = fgetc(f);
+  data[2] = 0;
+  return strtol(data, NULL, 16);
 }
 
-int load_obj(struct em8051 *aCPU, char *aFilename)
-{
-    FILE *f;
-    if (aFilename == 0 || aFilename[0] == 0)
-        return -1;
-    f = fopen(aFilename, "r");
-    if (!f) return -1;
-    if (fgetc(f) != ':')
-    {
-	  fclose(f);
-        return -2; // unsupported file format
+int load_obj(struct em8051* aCPU, char* aFilename) {
+  FILE* f;
+  if (aFilename == 0 || aFilename[0] == 0)
+    return -1;
+  f = fopen(aFilename, "r");
+  if (!f)
+    return -1;
+  if (fgetc(f) != ':') {
+    fclose(f);
+    return -2; // unsupported file format
+  }
+  while (!feof(f)) {
+    int recordlength;
+    int address;
+    int recordtype;
+    int checksum;
+    int i;
+    recordlength = readbyte(f);
+    address = readbyte(f);
+    address <<= 8;
+    address |= readbyte(f);
+    recordtype = readbyte(f);
+    if (recordtype == 1)
+      return 0; // we're done
+    if (recordtype != 0)
+      return -3;                                                              // unsupported record type
+    checksum = recordtype + recordlength + (address & 0xff) + (address >> 8); // final checksum = 1 + not(checksum)
+    for (i = 0; i < recordlength; i++) {
+      int data = readbyte(f);
+      checksum += data;
+      aCPU->mCodeMem[address + i] = data;
     }
-    while (!feof(f))
-    {
-        int recordlength;
-        int address;
-        int recordtype;
-        int checksum;
-        int i;
-        recordlength = readbyte(f);
-        address = readbyte(f);
-        address <<= 8;
-        address |= readbyte(f);
-        recordtype = readbyte(f);
-        if (recordtype == 1)
-            return 0; // we're done
-        if (recordtype != 0)
-            return -3; // unsupported record type
-        checksum = recordtype + recordlength + (address & 0xff) + (address >> 8); // final checksum = 1 + not(checksum)
-        for (i = 0; i < recordlength; i++)
-        {
-            int data = readbyte(f);
-            checksum += data;
-            aCPU->mCodeMem[address + i] = data;
-        }
-        i = readbyte(f);
-        checksum &= 0xff;
-        checksum = 256 - checksum;
-        if (i != (checksum & 0xff))
-            return -4; // checksum failure
-        while (fgetc(f) != ':' && !feof(f)) {} // skip newline
-    }
-	  fclose(f);
-    return -5;
+    i = readbyte(f);
+    checksum &= 0xff;
+    checksum = 256 - checksum;
+    if (i != (checksum & 0xff))
+      return -4; // checksum failure
+    while (fgetc(f) != ':' && !feof(f)) {
+    } // skip newline
+  }
+  fclose(f);
+  return -5;
 }
